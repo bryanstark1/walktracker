01202021fb00c1ebee030e92e0ed80ace08f58be
bryanstark1 <bryandstark@icloud.com>
2023-07-07 12:43:36 -0400

    Updated temporary model in views.py

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
+# Temporary walks model, until Class Based View is created
+walks = [
+    {'date': '2023-07-06', 'miles': 3, 'destination': 'Store', 'hours': 1.5},
+    {'date': '2023-07-07', 'miles': 4, 'destination': 'Supermarket', 'hours': 2}
+]
+
+
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
