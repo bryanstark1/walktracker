4487134292b5ae7b51ac1579fff002dd401ed3ae
bryanstark1 <bryandstark@icloud.com>
2023-07-07 12:38:55 -0400

    Defined index page URL and view function, added temporary model with 2 objects to views.py

--- main_app/views.py
+++ main_app/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def walks_index(request):
+    return render(request, 'walks/index.html', {
+        'walks': walks
+    })
